917b8a419984f1a549a0edaa28268e635c5d6696
shayed21 <shayedashraf21@gmail.com>
2025-06-22 16:00:22 +0600

    Delete Inflame-Invoice-Generator-main directory

--- Inflame-Invoice-Generator-main/src/utils/formatCurrency.js
+++
@@ -1,9 +0,0 @@
-
-export const formatCurrency = (amount, currencyCode = 'BDT', minimumFractionDigits = 2) => {
-  const locale = currencyCode === 'USD' ? 'en-US' : 'en-BD';
-  return new Intl.NumberFormat(locale, { style: 'currency', currency: currencyCode, minimumFractionDigits }).format(amount);
-};
-
-export const getCurrencySymbol = (currencyCode) => {
-  return formatCurrency(0, currencyCode).replace(/[\d.,\s]/g, '');
-};
